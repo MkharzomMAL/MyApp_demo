6aaed96686b36d4e8c3b49a179d412b396551a6a
MkharzomMAL <kharzom.mohammed.mal@gmail.com>
2023-05-10 11:04:04 +0800

    add class to controller and factory

--- student_app/resources/views/auth/register.blade.php
+++ student_app/resources/views/auth/register.blade.php
@@ -26,6 +26,20 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="class" class="col-md-4 col-form-label text-md-end">{{ __('Class') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="class" type="text" class="form-control @error('class') is-invalid @enderror" name="class" value="{{ old('class') }}" required autocomplete="class" autofocus>
+
+                                @error('class')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
